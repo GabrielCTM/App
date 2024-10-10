bd877d9b2d00ad41c05fe3f9901f8979e5a04168
Gabriel CTM <21151028@aguascalientes.tecnm.mx>
2024-10-10 17:45:59 -0600

    Added SnackBars, AlertDialogs and TopBar

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -63,7 +63,7 @@ import com.example.project1.ui.screens.MenuScreen
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
+        enableEdgeToEdge()
         setContent {
 
                 ComposeMultiScreenApp()
