914a759b8a63dbf64bda43cc983c3656dade71f9
Gabriel CTM <21151028@aguascalientes.tecnm.mx>
2024-10-24 21:44:41 -0600

    Feat :sparkles: (LoginScreen) Created Login screen interface

--- app/src/main/java/com/example/project1/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/project1/ui/screens/LoginScreen.kt
@@ -26,7 +26,7 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
-
+/**/
 @Composable
 fun LoginScreen(navController: NavController){
     Column(
